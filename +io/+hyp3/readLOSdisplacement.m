a6a1358b9a14cb9a6bc98447b2bd32991bdc6b19
zhurewitz <zhurewit@ucsd.edu>
2024-07-25 11:07:30 -0700

    HyP3: Read correlation

--- +io/+hyp3/readLOSdisplacement.m
+++ +io/+hyp3/readLOSdisplacement.m
@@ -1,6 +1,6 @@
 %% Read Line-of-Sight Displacement -- HyP3 Format
 
-function [LOSdisplacement,frameLat,frameLong]= ...
+function [LOSdisplacement,frameLat,frameLong,correlation]= ...
     readLOSdisplacement(filename)
 
 dL= 1/1200;
@@ -8,6 +8,7 @@ dL= 1/1200;
 metaData= io.hyp3.shortMetaData(filename);
 
 losfilename= fullfile(metaData.Fullname,strcat(metaData.Filename,'_los_disp.tif'));
+corrfilename= fullfile(metaData.Fullname,strcat(metaData.Filename,'_corr.tif'));
 
 boundingBox= io.readBoundingBox(filename);
 
@@ -21,7 +22,12 @@ frameLat= (floor(boundingBox(3)/dL)-1:ceil(boundingBox(4)/dL)+1)*dL;
 % Read raster and projection information
 [LOSproj,R]= readgeoraster(losfilename);
 LOSproj= 1000*LOSproj; % Convert from m to mm
-LOSproj(LOSproj == 0)= nan; % Missing data -> NaN
+I= LOSproj == 0;
+LOSproj(I)= nan; % Missing data -> NaN
+
+% Read correlation file
+[corrproj,~]= readgeoraster(corrfilename);
+corrproj(I)= nan; % Missing data -> NaN
 
 % Raster CRS local coordinates
 x= R.XWorldLimits(1):R.SampleSpacingInWorldX:R.XWorldLimits(2);
@@ -30,8 +36,9 @@ y= flip(R.YWorldLimits(1):R.SampleSpacingInWorldY:R.YWorldLimits(2));
 % Project query pixels into CRS
 [XQ,YQ]= projfwd(R.ProjectedCRS,LATQ,LONGQ);
 
-% Interpolate LOS
+% Interpolate LOS and correlation
 LOSdisplacement= interp2(x,y,LOSproj,XQ,YQ,'nearest');
+correlation= interp2(x,y,corrproj,XQ,YQ,'nearest');
 
 
 end
