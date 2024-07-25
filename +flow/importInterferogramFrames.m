c6f1544acf11b8b74a46a78af91e041191faded9
zhurewitz <zhurewit@ucsd.edu>
2024-07-25 11:30:00 -0700

    Moved write L1 product function into file

--- +flow/importInterferogramFrames.m
+++ +flow/importInterferogramFrames.m
@@ -186,7 +186,7 @@ for m= 1:length(Missions)
             
             %% Save Interferogram to HDF5 ProcessingStore File
             
-            h5.writeStitchedInterferogram(h5filename,mission,track,primaryDate(k),...
+            writeStitchedInterferogram(h5filename,mission,track,primaryDate(k),...
                 secondaryDate(k),LOS,trendMeta);
 
             fprintf('Mission %d/%d. Track %d/%d. Interferogram %d/%d saved. Elapsed time %0.1f min\n',...
@@ -194,5 +194,54 @@ for m= 1:length(Missions)
         end
     end
 end
+
+end
+
+
+
+
+
+
+
+
+
+%% Write L1 Stitched Interferogram to H5 Processing File
+
+function writeStitchedInterferogram(L1filename,Mission,Track,PrimaryDate,...
+    SecondaryDate,LOS,trendMeta)
+
+basename= '/interferogram/L1-stitched/';
+trackstr= strcat(Mission,'-',string(Track));
+path= fullfile(basename,trackstr);
+
+[PRIMARYDATE,SECONDARYDATE]= io.loadDates(L1filename,'L1',Mission,Track);
+
+if isempty(PRIMARYDATE)
+    k= 1;
+else
+    % Find where to place data if location already exists
+    I= PrimaryDate == PRIMARYDATE & SecondaryDate == SECONDARYDATE;
+    if any(I)
+        if sum(I) > 1
+            error('Data for this date pair somehow exists multiple times already')
+        end
+        k= find(I,1);
+    else
+        k= length(PRIMARYDATE)+ 1;
+    end
+end
+
+% Write to file
+h5.write2DInf(L1filename,path,'data',LOS,k,[300 300 1],3)
+h5.writeatts(L1filename,path,'data','units','mm','direction','LOS','orientation','upwards')
+
+h5.writeatts(L1filename,path,'','mission',Mission,'track',Track,'direction','PLACEHOLDER')
+
+h5.writeScalar(L1filename,path,'primaryDate',PrimaryDate,Inf,k)
+h5.writeScalar(L1filename,path,'secondaryDate',SecondaryDate,Inf,k)
+
+h5.write2DInf(L1filename,path,'trendMeta',trendMeta,k,[size(trendMeta) 1])
+h5.writeatts(L1filename,path,'trendMeta','units','mm')
+
 
 end
