d8acad115ce4eed768dc4ab78393e6ef42533add
zhurewitz <zhurewit@ucsd.edu>
2024-08-19 14:49:44 -0700

    KMZ: Subtract polyshape to account for holes

--- +kmz/subtract.m
+++ +kmz/subtract.m
@@ -1,7 +1,7 @@
 %% KMZ.SUBTRACT
 % Performs a subtraction operation on a pair of KMZ files
 
-function [x,y]= subtract(kmzfile1,kmzfile2,outputFile,outputName)
+function p= subtract(kmzfile1,kmzfile2,outputFile,outputName)
 
 arguments
     kmzfile1
@@ -25,12 +25,11 @@ result= subtract(pshape1,pshape2);
 warning on
 
 if nargout > 0
-    x= result.Vertices(:,1);
-    y= result.Vertices(:,2);
+    p= result;
 end
 
 if ~isempty(outputFile)
-    kmz.writeKMZPolygon(outputFile,x,y,outputName);
+    kmz.writePolyshape(outputFile,result,outputName);
 end
 
 end
